ac2b66ad44e1b0dacb283ba2965b2da906a6c6cd
AlexKif <sashaplakhotniy@gmail.com>
2019-12-12 02:31:20 +0200

    master: cleaned the project

--- src/index.js
+++ src/index.js
@@ -1,26 +1,10 @@
 import '@scss/main.scss'
-// import $ from 'jquery';
-//
-// $(".header").css("background", "yellow");
-
 // import jQuery from 'jquery'
+
+// $(".header").css("background", "yellow");
 // window.$ = window.jQuery = jQuery;
 
-import { Header } from '@/app/header';
-import { Footer } from '@/app/footer';
-import webpackgif from '@img/webpack.gif';
 
-// document.getElementById('webpack-gif').setAttribute('src', webpackgif)
-
-console.log(`***Header******`);
-let header = new Header();
-let firstHeading = header.getFirstHeading();
-console.log(firstHeading);
-
-console.log(`***footer******`);
-let footer = new Footer();
-let footerText = footer.getFooterText();
-console.log(footerText);
 
 // function renderPortfolioItems() {
 //     for (let i = 1; i<=18; i++) {
@@ -63,4 +47,4 @@ console.log(footerText);
 // }
 
 
-renderPortfolioItems();
+// renderPortfolioItems();
